42b58627dfa32bf623757abb325cd183136add2f
Davimesmo <davi.s.rodrigues2003@gmail.com>
2026-09-24 21:29:15 -0300

    feat: add comment routes

--- backend/app/api/routes/__init__.py
+++ backend/app/api/routes/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-
+from app.api.routes.comentarios import router as comentarios_router
 from app.api.routes.usuarios import router as usuarios_router
 
 api_router = APIRouter(prefix="/api")
 api_router.include_router(usuarios_router)
+api_router.include_router(comentarios_router)
